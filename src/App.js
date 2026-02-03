e125827b0a01e816ba1bcca33bedf6ca4e9626cc
sidsigma3 <sidsigma3@gmail.com>
2026-02-04 00:59:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/Homepage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
@@ -8,25 +7,13 @@ import CreateAccountPage from './pages/CreateAccountPage.jsx';
 import AccountPage from './pages/AccountPage.jsx';
 
 
-function ReloadRedirect() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/", { replace: true });
-  }, []);
-
-  return null;
-}
-
-
 function App() {
   return (
     <BrowserRouter>
-     <ReloadRedirect />
       <div className="app-wrapper">
         <div className="mobile-container">
           <Routes>
-            <Route path="/"  element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/signUp" element={<LoginPage />} />
             <Route path="/createAccount" element={<CreateAccountPage />} />
             <Route path="/accountSetting" element={<AccountPage />} />
